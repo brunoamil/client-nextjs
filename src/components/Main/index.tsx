7144b8b4dfa99b0331fc9e4e994a5fd346887245
Bruno Lima <bruno-amil@hotmail.com>
2020-11-03 21:20:38 -0300

    configurando o styled component

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,15 @@
+import * as S from './styles'
+
 const Main = () => (
-  <main>
-    <h2>React Avançado</h2>
-  </main>
+  <S.Wrapper>
+    <S.Logo src="/img/logo.svg" alt="Imagem de um atomo e react" />
+    <S.Title>React Avançado</S.Title>
+    <S.Description>TypeScript</S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Ilustração de um garoto sentado no pc"
+    />
+  </S.Wrapper>
 )
 
 export default Main
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,5 @@
-import Head from 'next/head'
+import Main from 'components/Main'
 
 export default function Home() {
-  return (
-    <Head>
-      <p>teste</p>
-    </Head>
-  )
+  return <Main />
 }
