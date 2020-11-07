367b70144725c869310a7da7032bfd9b0731fc57
Bruno Lima <bruno-amil@hotmail.com>
2020-11-07 16:14:17 -0300

    criando  o theme do site

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,22 @@
 import { AppProps } from 'next/app'
 import Head from 'next/head'
+import { ThemeProvider } from 'styled-components'
+
 import GlobalStyles from 'styles/global'
+import theme from 'styles/theme'
 
 function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Head>
-        <title>NextJS com TS</title>
+        <title>Won Games</title>
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <meta name="description" content="Um projeto de NEXTJS com TS" />
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   )
 }
 
